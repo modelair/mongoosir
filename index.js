b9186dd2a3bd3ede60a2c617090efd0421de3281
modelair <cheaterwork@gmail.com>
2018-09-27 19:49:38 +1200

    move fields and patterns to modelair

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const fields = require('./src/fields')
 const mongoosir = require('./src/mongoosir')
-const patterns = require('./src/patterns')
-
-
 module.exports = mongoosir
-module.exports.fields = fields
-module.exports.patterns = patterns
-module.exports.mongoosir = mongoosir
